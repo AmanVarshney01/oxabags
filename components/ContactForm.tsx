338bc8a9eea5d4b16674b760ed0a31b32017471e
Aman Varshney <av.amanvarshney11@gmail.com>
2023-12-27 18:59:46 +0530

    Commented out unused code in ContactForm component

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,40 +1,40 @@
-"use client";
-import { useState } from "react";
+// "use client";
+// import { useState } from "react";
 import { Button } from "./ui/button";
 
 export default function ContactForm() {
-  const [formData, setFormData] = useState<any>({ phone: "" });
-  const encode = (data: any) => {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]),
-      )
-      .join("&");
-  };
+//   const [formData, setFormData] = useState<any>({ phone: "" });
+//   const encode = (data: any) => {
+//     return Object.keys(data)
+//       .map(
+//         (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]),
+//       )
+//       .join("&");
+//   };
 
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  }
+//   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+//     setFormData({ ...formData, [e.target.name]: e.target.value });
+//   }
 
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    const form = e.target as HTMLFormElement;
-    const formData: any = new FormData(form);
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...formData }),
-    })
-      .then(() => {
-        alert("Thank you for contacting us. We will get back to you soon.");
-        form.reset();
-      })
-      .catch((error) => alert(error));
-  }
+//   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+//     e.preventDefault();
+//     const form = e.target as HTMLFormElement;
+//     const formData: any = new FormData(form);
+//     fetch("/", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//       body: encode({ "form-name": "contact", ...formData }),
+//     })
+//       .then(() => {
+//         alert("Thank you for contacting us. We will get back to you soon.");
+//         form.reset();
+//       })
+//       .catch((error) => alert(error));
+//   }
 
   return (
     <form
-      onSubmit={handleSubmit}
+      //   onSubmit={handleSubmit}
       name="contact"
       method="POST"
       data-netlify="true"
@@ -52,7 +52,7 @@ export default function ContactForm() {
         placeholder="Enter your phone number"
         className="mt-3 w-full rounded-lg border-2 border-white px-3 py-2 text-black transition-colors duration-200 focus:border-blue-500 focus:outline-none"
         required
-        onChange={handleChange}
+        // onChange={handleChange}
       />
       <Button type="submit" className="mt-3 w-full" variant="secondary">
         Submit
