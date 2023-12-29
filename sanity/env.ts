6bd1ed26a4c76b5a868f8622db96f956d605cd8d
Aman Varshney <av.amanvarshney11@gmail.com>
2023-12-29 16:13:38 +0530

    Update useCdn flag to true in sanity/env.ts

--- sanity/env.ts
+++ sanity/env.ts
@@ -11,7 +11,7 @@ export const projectId = assertValue(
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
-export const useCdn = false
+export const useCdn = true
 
 export const token = assertValue(
   process.env.NEXT_PUBLIC_SANITY_TOKEN,
