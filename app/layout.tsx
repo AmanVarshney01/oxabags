be41e0b2b6d37f30175e3e668640c931d356499b
Aman Varshney <av.amanvarshney11@gmail.com>
2024-01-06 03:59:04 +0530

    Update default theme to "system" in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
