febbec573f71ea5101cb2572c72bf67eaa8709be
Aman Varshney <av.amanvarshney11@gmail.com>
2024-01-30 18:22:22 +0530

    Update min-h-screen to min-h-svh in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "min-h-svh bg-background font-sans antialiased",
           fontSans.variable,
         )}
       >
