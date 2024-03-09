54c308528199191d30931f0d09e7bc962f430150
Aman Varshney <av.amanvarshney11@gmail.com>
2024-03-09 12:11:21 +0530

    Delete unused actions.ts file and remove import of sendInvoice function from cart/page.tsx

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -5,7 +5,6 @@ import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Loader2 } from "lucide-react";
-import { sendInvoice } from "../actions";
 import {
   Form,
   FormControl,
