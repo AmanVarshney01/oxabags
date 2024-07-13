c3030943a7cec4cc938a58d61bbf00067f919ce7
Aman Varshney <av.amanvarshney11@gmail.com>
2024-07-13 21:41:46 +0530

    Update environment variables

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,25 +2,23 @@
  * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...index]]\page.tsx` route
  */
 
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
+import { visionTool } from "@sanity/vision";
+import { defineConfig } from "sanity";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 // import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schema'
+import { schema } from "./sanity/schema";
 
 export default defineConfig({
   title: "Amanasia Studio",
   // basePath: '/studio',
-  projectId: 'du6qptuy',
-  dataset: 'production',
+  projectId: "du6qptuy",
+  dataset: "production",
   // Add and edit the content schema in the './sanity/schema' folder
   schema,
   plugins: [
-    deskTool(),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: '2021-03-25'}),
+    visionTool(),
   ],
-})
+});
